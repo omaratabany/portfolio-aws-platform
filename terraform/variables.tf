ea7fbfc2a146376c62c2b8e5fed5bc721813ba07
omaratabany <123808165+omaratabany@users.noreply.github.com>
2026-04-19 23:38:28 +0400

    Terraform first push
    terraform conditions and versions setup

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS Region for all resources"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "project" {
+  description = "project used in resource naming and tagging"
+  type        = string
+  default     = "portfolio-platofrm"
+}
+
+variable "environment" {
+  description = "deployment environment"
+  type        = string
+  default     = "dev"
+
+  validation {
+    condition     = contains(["dev", "prod"], var.environment)
+    error_message = "Environment must be dev or prod"
+  }
+}
+
